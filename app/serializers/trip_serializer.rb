a9b6c57acf761a2455ecbad7cf020275101a0c29
Marcus Vieira <marcusbrunovieira@gmail.com>
2014-12-17 12:14:11 -0200

    add attributes to `TripSerializer`

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,3 +1,21 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :start, :end
+  include ActionView::Helpers::DateHelper
+
+  attributes :id, :start, :end,
+             :wishlist_count, :recommendation_count,
+             :created_at
+
+  has_one :destination
+
+  def wishlist_count
+    0
+  end
+
+  def recommendation_count
+    0
+  end
+
+  def created_at
+    time_ago_in_words(object.created_at)
+  end
 end
