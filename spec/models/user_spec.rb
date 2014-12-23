ff335c9ce2b12d7967cbf85f8766af04d0f3a0e6
Marcus Vieira <marcusbrunovieira@gmail.com>
2014-12-23 17:18:55 -0200

    remove email uniqueness validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,18 +10,6 @@ RSpec.describe User, type: :model do
   context 'when data is invalid' do
     it { expect(build(:user, fb_id: nil)).to validate_presence_of(:fb_id) }
     it { expect(build(:user, fb_token: nil)).to validate_presence_of(:fb_token) }
-
-    describe '#email' do
-      it { expect(build(:user, email: nil)).to validate_presence_of(:email) }
-
-      it 'is invalid with duplicates' do
-        create(:user)
-        user = build(:user)
-
-        user.valid?
-        expect(user.errors[:email]).to include 'has already been taken'
-      end
-    end
   end
 
   context 'when data is valid' do
