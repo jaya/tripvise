6ac882304293863be758f7b023234621a110f362
Marcus Vieira <marcusbrunovieira@gmail.com>
2015-01-08 14:27:50 -0200

    remove attribute `code` from `Trip`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108161116) do
+ActiveRecord::Schema.define(version: 20150108162623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(version: 20150108161116) do
     t.datetime "updated_at"
     t.integer  "destination_id"
     t.integer  "user_id"
-    t.string   "code"
     t.integer  "recommendation_type_id"
   end
 
