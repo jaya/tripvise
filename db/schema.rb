45b309a246592cc53c2f6982e7cdb97b5009719b
Marcus Vieira <marcusbrunovieira@gmail.com>
2015-01-15 14:05:41 -0200

    add `city` to `Place`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108162623) do
+ActiveRecord::Schema.define(version: 20150115160426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150108162623) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "city"
   end
 
   create_table "recommendation_types", force: true do |t|
--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :place do
     name 'Aracaju'
+    city 'Aracaju, State of Sergipe, Brazil'
   end
 end
