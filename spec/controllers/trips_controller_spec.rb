b9090663073758ea2dcd3f51e08ac9e19c324e8b
Marcus Vieira <marcusbrunovieira@gmail.com>
2015-01-16 16:28:24 -0200

    create `trip#recommendations`; add tests

--- spec/controllers/trips_controller_spec.rb
+++ spec/controllers/trips_controller_spec.rb
@@ -80,4 +80,57 @@ RSpec.describe TripsController, type: :controller do
       end
     end
   end
+
+  describe '#recommendations' do
+    before do
+      create(:recommendation, trip: trip) if create_recommendation?
+
+      token = 'Token token=' + user[:fb_token]
+      request.headers['HTTP_AUTHORIZATION'] = token
+      get :recommendations, format: :json, id: trip[:id]
+    end
+    let(:json_response) { JSON.parse(response.body) }
+
+    context 'with valid data' do
+      let(:user) { create(:user) }
+      let(:trip) { create(:trip, user: user) }
+      let(:create_recommendation?) { true }
+
+      it 'responds with 200' do
+        expect(response).to have_http_status :ok
+      end
+
+      describe 'JSON response' do
+        it 'has objects' do
+          expect(json_response).to_not be_nil
+        end
+
+        it 'has recommendations' do
+          expect(json_response['recommendations'].count).to be > 0
+        end
+      end
+    end
+
+    context 'with invalid data' do
+      context 'when user is invalid' do
+        let(:user) { build(:user, id: 123, fb_token: '123123A') }
+        let(:trip) { create(:trip) }
+        let(:create_recommendation?) { false }
+
+        it 'responds with 401' do
+          expect(response).to have_http_status :unauthorized
+        end
+      end
+
+      context 'when trip is invalid' do
+        let(:user) { create(:user) }
+        let(:trip) { build(:trip, id: 10_000, user: user) }
+        let(:create_recommendation?) { false }
+
+        it 'responds with 400' do
+          expect(response).to have_http_status :bad_request
+        end
+      end
+    end
+  end
 end
