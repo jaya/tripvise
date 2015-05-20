4db96ffee0d741656c60f94432221a66701fd00f
Augusto Goldoni <augusto@jaya-apps.com>
2015-05-20 17:25:40 -0300

    migration to update all register with private true.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519212227) do
+ActiveRecord::Schema.define(version: 20150520201758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
